c46d685bd95ea7452d5311ece78fb7d351d497c6
Livia2710 <130663916+Livia2710@users.noreply.github.com>
2026-06-14 10:44:39 -0300

    Criando o projeto

---
+++ millie/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { EB_Garamond } from "next/font/google";
+import "./globals.css";
+
+const ebGaramond = EB_Garamond({
+  variable: "--font-eb-garamond",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "Millie Munds",
+  description: "Escola dos Mil Mundos",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={ebGaramond.variable}>{children}</body>
+    </html>
+  );
+}

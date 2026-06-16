c33b87ea048e8fb0ff349cd95df301d662830e7d
Livia2710 <130663916+Livia2710@users.noreply.github.com>
2026-06-15 23:21:30 -0300

    Update CharacterFilters.tsx

--- millie/componentes/personagens/CharacterFilters.tsx
+++ millie/componentes/personagens/CharacterFilters.tsx
@@ -66,7 +66,7 @@ export default function CharacterFilters({
       className={
         isMobile
           ? "w-full"
-          : "py-10"
+          : "md:border-r md:border-bege-escuro/25 md:px-4 py-10"
       }
     >
       <FilterGroup title="Categoria" compact={isMobile}>
